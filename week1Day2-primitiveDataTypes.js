9d86adb259eec7362878370209eeae1fb99f5378
getchandra <getchandra.ssn@gmail.com>
2026-07-08 16:28:56 +0530

    Add files via upload
    Week1_Day2_Home Assignment

---
+++ week1Day2-primitiveDataTypes.js
@@ -0,0 +1,25 @@
+/* Home Assignment on Primitive Data Types */
+/* String data type */
+let userName = "Varma";
+console.log(userName);
+console.log(typeof userName);
+
+let organization = "Cognizant";
+console.log(organization);
+console.log(typeof organization);
+
+/* Number data type */
+let contactNumber = 9677198425;
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+/* Boolean data type */
+let knowsAutomation = false;
+console.log(contactNumber);
+console.log(typeof knowsAutomation);
+
+/* Undefined data type */
+let usesPlaywright;
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
