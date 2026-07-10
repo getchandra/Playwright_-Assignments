4a841841f1ceb39f53934a62bb4180f7be6f83dd
getchandra <getchandra.ssn@gmail.com>
2026-07-10 15:57:29 +0530

    Add files via upload

---
+++ week1Day4-loops-printSquaresInReverseOrder.js
@@ -0,0 +1,5 @@
+/* Program to prints the squares of numbers from 1 to 10 in reverse order */
+for (let i = 10; i >= 1; i--) {
+    console.log(i * i);
+    console.log("This is the square of " + i);
+}
